6eb257ae5120072a4bcbf86c2f6587ed633d5962
Ot92 <onalethatatautona@gmail.com>
2024-07-21 20:00:09 +0200

    Update message-service.ts

--- govauctionplatform/src/services/message-service.ts
+++ govauctionplatform/src/services/message-service.ts
@@ -5,10 +5,14 @@ import { EMessageSortType, ESortOrderType, LIST_LIMIT_NUMBER, MAX_LIST_LIMIT_NUM
 import { IMessage, IMessageInput, Message } from "../models/message-model";
 
 /**
- * Add a message.
- * 
- * @param input
- * @returns 
+ * Creates a new message and saves it to the database.
+ *
+ * @param {IBidder} currentUser The currently logged-in user object.
+ * @param {IMessageInput} input The message data to be created.
+ * @returns {Promise<IMessage>} A promise that resolves to the newly created message document.
+ * @throws {NotFoundError} Thrown if the item referenced in the input is not found.
+ * @throws {ForbiddenError} Thrown if the current user is not an eligible bidder for the item.
+ * @throws {Error} Any other errors encountered during message creation.
  */
 async function createMessage(currentUser: IBidder, input: IMessageInput): Promise<IMessage> {
   try {
@@ -36,11 +40,20 @@ async function createMessage(currentUser: IBidder, input: IMessageInput): Promis
 }
 
 /**
- * Get messages.
- * 
- * @param conditions
- * @param projection
- * @returns 
+ * Retrieves a list of messages based on provided conditions.
+ *
+ * @param {Map<string, any>} conditions A map containing filtering parameters for message retrieval.
+ *  - `itemId`: (Optional) The ID of the item to filter messages by.
+ *  - `startDate`: (Optional) The start date for filtering messages by creation date.
+ *  - `endDate`: (Optional) The end date for filtering messages by creation date.
+ *  - `limit`: (Optional) The maximum number of messages to return (must be between 1 and MAX_LIST_LIMIT_NUMBER).
+ *  - `sortBy`: (Optional) The field to sort messages by (currently only supports 'DATE').
+ *  - `sortOrder`: (Optional) The sort order (either 'ASC' or 'DESC').
+ *  - `lastDocumentId`: (Optional) Used for pagination, specifies the ID of the last message retrieved in the previous request.
+ * @param {object} projection (Optional) A projection object to specify which fields to include in the retrieved messages.
+ * @returns {Promise<IMessage[]>} A promise that resolves to an array of message documents matching the search criteria.
+ * @throws {ForbiddenError} Thrown if the provided `limit` exceeds the allowed maximum.
+ * @throws {Error} Any other errors encountered during message retrieval.
  */
 async function getMessages(conditions: Map<string, any>, projection?: any): Promise<IMessage[]> {
   try {
@@ -100,8 +113,26 @@ async function getMessages(conditions: Map<string, any>, projection?: any): Prom
   }
 }
 
+/**
+ * Updates the `isRead` flag of a message to `true`.
+ *
+ * @param {string} messageId The ID of the message to update.
+ * @returns {Promise<IMessage | null>} A promise that resolves to the updated message document or null if not found.
+ * @throws {Error} Any errors encountered during the update process.
+ */
+async function updateIsRead(messageId: string): Promise<IMessage | null> {
+  try {
+    const message = await Message.findByIdAndUpdate(messageId, { $set: { isRead: true } }, { new: true });
+    return message;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
 // Export default
 export default {
   createMessage,
-  getMessages
+  getMessages,
+  updateIsRead
 } as const;
