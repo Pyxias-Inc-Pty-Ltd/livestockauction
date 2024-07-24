3e925cb206071bbf919d6bee422c023b2e3fb640
Ot92 <onalethatatautona@gmail.com>
2024-07-24 07:25:24 +0200

    Create forum-service.ts

---
+++ govauctionplatform/src/services/forum-service.ts
@@ -0,0 +1,199 @@
+import { IForum, Forum } from '../models/forum-model';
+import { IForumComment, ForumComment } from '../models/forum-model';
+import { ForbiddenError, NotFoundError } from '../shared/errors';
+import { ClientSession, Schema, startSession } from 'mongoose';
+import { ESortOrderType, LIST_LIMIT_NUMBER, MAX_LIST_LIMIT_NUMBER } from '../globals';
+import { isMongoId } from 'validator';
+import { IAdmin, IBidder } from '../models/user-model';
+
+/**
+ * Create a forum.
+ *
+ * @param input
+ * @returns
+ */
+async function createForum(input: IForum): Promise<IForum> {
+    let sess: ClientSession | null = null;
+
+    try {
+        const newForum = new Forum(input);
+
+        // Start session and mongo acid transaction
+        sess = await startSession();
+
+        await sess.withTransaction(async () => {
+            await newForum.save({ session: sess });
+        });
+
+        return newForum;
+    } catch (error) {
+        throw error;
+    } finally {
+        if (sess) {
+            // End session
+            await sess.endSession();
+        }
+    }
+}
+
+/**
+ * Get a forum by auction ID.
+ *
+ * @param auctionId
+ * @param projection
+ * @returns
+ */
+async function getForumByAuctionId(auctionId: string | Schema.Types.ObjectId, projection?: any): Promise<IForum | null> {
+    try {
+        if (isMongoId(auctionId.toString())) {
+            const forum = await Forum.findOne({ auctionId }, projection);
+            return forum;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+/**
+ * Update a forum by ID.
+ *
+ * @param id
+ * @param updateData
+ * @returns
+ */
+async function updateForum(id: string | Schema.Types.ObjectId, updateData: Partial<IForum>): Promise<IForum | null> {
+    try {
+        if (isMongoId(id.toString())) {
+            const forum = await Forum.findByIdAndUpdate(id, updateData, { new: true }).exec();
+            return forum;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+/**
+ * Delete a forum by ID.
+ *
+ * @param id
+ * @returns
+ */
+async function deleteForum(id: string | Schema.Types.ObjectId): Promise<IForum | null> {
+    try {
+        if (isMongoId(id.toString())) {
+            const forum = await Forum.findByIdAndDelete(id).exec();
+            return forum;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+/**
+ * Create a forum comment.
+ *
+ * @param input
+ * @returns
+ */
+async function createForumComment(currentUser: IAdmin | IBidder, input: IForumComment): Promise<IForumComment> {
+    let sess: ClientSession | null = null;
+
+    try {
+        input.authorId = currentUser.id;
+        // TODO: Check if bidder is in list of eligible items for an of the items under the supplied action
+        const newComment = new ForumComment(input);
+
+        // Start session and mongo acid transaction
+        sess = await startSession();
+
+        await sess.withTransaction(async () => {
+            await newComment.save({ session: sess });
+        });
+
+        return newComment;
+    } catch (error) {
+        throw error;
+    } finally {
+        if (sess) {
+            // End session
+            await sess.endSession();
+        }
+    }
+}
+
+/**
+ * Get a forum comment by ID.
+ *
+ * @param id
+ * @returns
+ */
+async function getForumCommentById(id: string | Schema.Types.ObjectId): Promise<IForumComment | null> {
+    try {
+        if (isMongoId(id.toString())) {
+            const comment = await ForumComment.findById(id).exec();
+            return comment;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+/**
+ * Update a forum comment by ID.
+ *
+ * @param id
+ * @param updateData
+ * @returns
+ */
+async function updateForumComment(id: string | Schema.Types.ObjectId, updateData: Partial<IForumComment>): Promise<IForumComment | null> {
+    try {
+        if (isMongoId(id.toString())) {
+            const comment = await ForumComment.findByIdAndUpdate(id, updateData, { new: true }).exec();
+            return comment;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+/**
+ * Delete a forum comment by ID.
+ *
+ * @param id
+ * @returns
+ */
+async function deleteForumComment(id: string | Schema.Types.ObjectId): Promise<IForumComment | null> {
+    try {
+        if (isMongoId(id.toString())) {
+            // TODO: Make sure a user can only delete their own comment if they are not an admin
+            const comment = await ForumComment.findByIdAndDelete(id).exec();
+            return comment;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Export default
+export default {
+    createForum,
+    getForumByAuctionId,
+    updateForum,
+    deleteForum,
+    createForumComment,
+    getForumCommentById,
+    updateForumComment,
+    deleteForumComment
+} as const;
