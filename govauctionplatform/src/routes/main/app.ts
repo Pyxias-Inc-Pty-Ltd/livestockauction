454bb6f3e74845031c1bb41f683abd6a78e7c4ac
Ot92 <onalethatatautona@gmail.com>
2024-07-25 11:44:11 +0200

    Update app.ts

--- govauctionplatform/src/routes/main/app.ts
+++ govauctionplatform/src/routes/main/app.ts
@@ -7,6 +7,7 @@ import categoryRouter from '../category-router';
 import auctionRouter from '../auction-router';
 import breedRouter from '../breed-router';
 import forumRouter from '../forum-router';
+import notificationTriggerRouter from '../notification-trigger-router';
 
 // Export the base-router
 const baseRouter = Router();
@@ -20,6 +21,7 @@ baseRouter.use('/transactions', transactionRouter);
 baseRouter.use('/categories', categoryRouter);
 baseRouter.use('/breeds', breedRouter);
 baseRouter.use('/forums', forumRouter);
+baseRouter.use('/notificationTriggers', notificationTriggerRouter);
 
 // Export default.
 export default baseRouter;
