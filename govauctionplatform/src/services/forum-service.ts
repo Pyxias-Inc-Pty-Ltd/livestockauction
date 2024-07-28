e3963b2b6513f78eeece4a18a7b70819be0041d6
Ot92 <onalethatatautona@gmail.com>
2024-07-28 13:59:57 +0200

    Update forum-service.ts

--- govauctionplatform/src/services/forum-service.ts
+++ govauctionplatform/src/services/forum-service.ts
@@ -1,8 +1,8 @@
 import { IForum, Forum, IForumInput } from '../models/forum-model';
-import { IForumComment, ForumComment } from '../models/forum-model';
+import { IForumComment, ForumComment, IForumCommentInput } from '../models/forum-model';
 import { ForbiddenError, NotFoundError } from '../shared/errors';
 import { ClientSession, Schema, startSession } from 'mongoose';
-import { ESortOrderType, LIST_LIMIT_NUMBER, MAX_LIST_LIMIT_NUMBER } from '../globals';
+import { EForumCommentSortType, ESortOrderType, LIST_LIMIT_NUMBER, MAX_LIST_LIMIT_NUMBER } from '../globals';
 import { isMongoId } from 'validator';
 import { IAdmin, IBidder } from '../models/user-model';
 import userService from "./user-service";
@@ -120,7 +120,7 @@ async function deleteForum(id: string | Schema.Types.ObjectId): Promise<IForum |
  * @param input
  * @returns
  */
-async function createForumComment(currentUser: IAdmin | IBidder, input: IForumComment): Promise<IForumComment> {
+async function createForumComment(currentUser: IAdmin | IBidder, input: IForumCommentInput): Promise<IForumComment> {
     let sess: ClientSession | null = null;
 
     try {
@@ -205,6 +205,70 @@ async function deleteForumComment(id: string | Schema.Types.ObjectId): Promise<I
     }
 }
 
+/**
+ * Retrieves a list of forum comments based on provided conditions.
+ *
+ * @param conditions
+ * @param projection
+ * @returns
+ */
+async function getForumComments(conditions: Map<string, any>, projection?: any): Promise<IForumComment[]> {
+  try {
+    let _limit: number = LIST_LIMIT_NUMBER;
+
+    // Set custom limit
+    if (conditions.get('limit') && conditions.get('limit') >= 1) {
+      if (conditions.get('limit') > MAX_LIST_LIMIT_NUMBER) {
+        throw new ForbiddenError(`limit must not exceed ${MAX_LIST_LIMIT_NUMBER}`);
+      }
+      _limit = conditions.get('limit');
+    }
+
+    // Query builder
+    const q = ForumComment.find({}, projection);
+
+    // Filters
+    if (conditions.get('forumId')) {
+      q.where({ forumId: conditions.get('forumId') });
+    }
+
+    // Range
+    if (conditions.get('startDate') && conditions.get('endDate')) {
+      q.and([{ 'createdDate': { $gte: new Date(conditions.get('startDate')) } }, { 'createdDate': { $lte: new Date(conditions.get('endDate')) } }]);
+    } else if (conditions.get('startDate')) {
+      q.where({ 'createdDate': { $gte: new Date(conditions.get('startDate')) } });
+    } else if (conditions.get('endDate')) {
+      q.where({ 'createdDate': { $lte: new Date(conditions.get('endDate')) } });
+    }
+
+    // Sort
+    if (conditions.get('sortBy')) {
+      if (conditions.get('sortBy') === EForumCommentSortType.DATE) {
+        q.sort({'_id': conditions.get('sortOrder')});
+      }
+    }
+
+    // Pagination
+    if (conditions.get('lastDocumentId')) {
+      // Check the sort order
+      if (conditions.get('sortOrder') === ESortOrderType.ASC || conditions.get('sortOrder') === ESortOrderType.asc) {
+        q.where("_id").gt(conditions.get('lastDocumentId'));
+      } else {
+        q.where("_id").lt(conditions.get('lastDocumentId'));
+      }
+    }
+
+    // Limit
+    q.limit(_limit);
+
+    return await q;
+
+  } catch (error) {
+    // Rethrow error
+    throw error;
+  }
+}
+
 // Export default
 export default {
     createForum,
@@ -215,5 +279,6 @@ export default {
     createForumComment,
     getForumCommentById,
     updateForumComment,
-    deleteForumComment
+    deleteForumComment,
+    getForumComments
 } as const;
