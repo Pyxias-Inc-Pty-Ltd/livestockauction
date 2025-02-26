261af1fcea0184d8d5fb28acc9f6fe6ccaf88cd9
Ot92 <onalethatatautona@gmail.com>
2025-02-27 01:54:53 +0200

    Update index.ts

--- govauctionplatform/src/index.ts
+++ govauctionplatform/src/index.ts
@@ -14,6 +14,8 @@ import * as admin from 'firebase-admin';
 import authHandler from './handlers/auth-handler';
 import { CustomError } from './shared/errors';
 import { runSchedulers } from './scheduler';
+import { join } from 'path';
+import { readFileSync } from 'fs-extra';
 
 const { OK, INTERNAL_SERVER_ERROR, CREATED } = StatusCodes;
 
@@ -35,6 +37,9 @@ export const io = new Server(httpServer, {
     origin: ['http://localhost:5173', SERVICE_URLS.clientURI]
   }
 });
+
+// Load the public key once at startup
+export const webhookSignaturePublicKey = readFileSync(join(__dirname, "../shared/sec/public_key.pem"), "utf8");
 
 // Check for auth on handshake
 io.use(async (socket: any, next: any) => {
@@ -190,6 +195,7 @@ httpServer.listen(port, async () => {
     logger.info(serverStartMsg + port);
 
     // Start connection to mongodb
+    logger.info(SERVICE_URLS.mongoDBURI);
     await connect(SERVICE_URLS.mongoDBURI);
     logger.info('Connection to mongodb established');
 
