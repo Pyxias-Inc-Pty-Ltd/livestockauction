6f708d094c876606b24902c263d46c43e8bda082
Ot92 <onalethatatautona@gmail.com>
2026-07-11 17:49:36 +0200

    Update user-model.ts

--- govauctionplatform/src/models/user-model.ts
+++ govauctionplatform/src/models/user-model.ts
@@ -29,6 +29,7 @@ export interface ISeller extends IUser {
   name: string;
   allowedRequiredAttributes: Schema.Types.ObjectId[];
   paygateId?: string;
+  payhostEncryptionKey?: string;
 }
 
 export interface ISellerInput {
@@ -505,6 +506,7 @@ const sellerSchema = new Schema<ISeller>({
   sectorType: { type: String, default: ESectorType.GOVERNMENT, enum: [ESectorType.PRIVATE, ESectorType.GOVERNMENT], required: true },
   allowedRequiredAttributes: { type: [{ type: Schema.Types.ObjectId, ref: EModels.REQUIRED_ATTRIBUTE }], default: [] },
   paygateId: { type: String, trim: true },
+  payhostEncryptionKey: { type: String, trim: true },
   email: {type: String, unique: true, required: true, validate: {
     msg: 'Valid email must be supplied.',
       validator: function (v: string): boolean {
